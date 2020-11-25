3fb09cc8697d9a92ec9f6d8d6c9acaa7e9f20615
hannahjorich <hannah.jorich@gmail.com>
2020-11-24 21:28:07 -0700

    added project links

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ import Header from './components/Header';
 import About from './components/About';
 import Work from './components/work';
 import Footer from './components/Footer';
-import Navbar from './components/Navbar'
+import Navbar from './components/Navbar';
 
 function App() {
   return (
--- src/yourdata.js
+++ src/yourdata.js
@@ -1,83 +1,86 @@
-export default
+export default {
+  //(Please Do Not Remove The comma(,) after every variable)
+  //Change The Website Template
+  name: "hannahjorich.",
+  headerTagline: [
+    //Line 1 For Header
+    "Hi, I am Hannah Jorich.",
+    //Line 2 For Header
+    "Content Creator. ",
+    //Line 3 For Header
+    "from Colorado.",
+  ],
+  //Contact Email
+  contactEmail: "hannah.jorich@gmail.com",
+  // Add Your About Text Here
+  abouttext:
+    "I am a digital content creator pursuing a certificate in full stack development. I am known for my creativity, strategic problem solving, and my ability to be a leader among my team. I hold a Bachelor's degree in Communications, Master’s in Sports Management and now I am furthering my education by earning a certificate in full stack development from the University of Denver, with a skillset in HTML, CSS, JavaScript, Node.js and React.js. I’m excited to leverage my skill set in a fast-paced, quality-driven team that enhances better experiences on the web.",
+  aboutImage:
+    "https://images.unsplash.com/photo-1483197452165-7abc4b248905?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+  //Change This To Hide The Image of About Section (True Or False)
+  ShowAboutImage: true, //true or false (Change Here)
+  // Change Projects Here
+  projects: [
     {
-        //(Please Do Not Remove The comma(,) after every variable)
-        //Change The Website Template
-        name :'hannahjorich.',
-        headerTagline: [//Line 1 For Header
-                        'Hi, I am Hannah Jorich.',
-                        //Line 2 For Header
-                        'Content Creator. ',
-                        //Line 3 For Header
-                        'from Colorado.'
-    ],
-        //Contact Email
-        contactEmail:'hannah.jorich@gmail.com',
-        // Add Your About Text Here
-        abouttext: "I am a digital content creator pursuing a certificate in full stack development. I am known for my creativity, strategic problem solving, and my ability to be a leader among my team. I hold a Bachelor's degree in Communications, Master’s in Sports Management and now I am furthering my education by earning a certificate in full stack development from the University of Denver, with a skillset in HTML, CSS, JavaScript, Node.js and React.js. I’m excited to leverage my skill set in a fast-paced, quality-driven team that enhances better experiences on the web.",
-        aboutImage:'https://images.unsplash.com/photo-1483197452165-7abc4b248905?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60',
-       //Change This To Hide The Image of About Section (True Or False)
-       ShowAboutImage:true,//true or false (Change Here)
-       // Change Projects Here 
-       projects:[
-           {
-            id: 1,//DO NOT CHANGE THIS (Please)😅
-            title:'Trails App', //Project Title - Add Your Project Title Here
-             service:'Web Development', // Add Your Service Type Here
-            //Project Image - Add Your Project Image Here
-             imageSrc:"https://images.unsplash.com/photo-1424169292451-7c28a6053189?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-             //Project URL - Add Your Project Url Here
-             url:'https://pprewitt.github.io/TrailsApp/'
-            },
-            {
-                id: 2,//DO NOT CHANGE THIS (Please)😅
-                title: 'Book Finder',
-                service: 'Website Development',
-                imageSrc: "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'https://book-worms-book-finder.herokuapp.com/'
-            },
-            { 
-                id: 3,//DO NOT CHANGE THIS (Please)😅
-                title: 'Burger App',
-                service: 'Website Development',
-                imageSrc: "https://images.unsplash.com/photo-1530554764233-e79e16c91d08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
-                url: 'https://burger-app-jorich.herokuapp.com/'
-            },
-            {
-                id: 4,//DO NOT CHANGE THIS (Please)😅
-                title: 'Workout Tracker',
-                service: 'Website Development',
-                imageSrc: "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=600&q=60",
-                url: 'https://workout-tracker-jorich.herokuapp.com/?id=5fa20d8fe149100017db825f'
-           }
-
-                    /*
-
-                    If You Want To Add More Project just Copy and Paste This At The End (Update the id Respectively)
-                ,{
-                id: 5,
-                title: 'Project Five',
-                service: 'Something Amazing',
-                imageSrc: "",
-                url: ''
-            }
-                */
-        ],
-        social: [
-            // Add Or Remove The Link Accordingly
-            {   name:'Github',
-                url:'https://github.com/chetanverma16'},
-            {
-                name: 'Behance',
-                url: 'https://www.behance.net/chetanverma'
-            },
-            {
-                name: 'Dribbble',
-                url: 'https://dribbble.com/chetanverma'
-            },
-            {
-                name: 'Instagram',
-                url: 'https://www.instagram.com/cv.uidesign/'
-            }
-
-        ]
-    }
+      id: 1, //DO NOT CHANGE THIS (Please)😅
+      title: "Trails App", //Project Title - Add Your Project Title Here
+      service: "Web Development", // Add Your Service Type Here
+      //Project Image - Add Your Project Image Here
+      imageSrc:
+        "https://images.unsplash.com/photo-1424169292451-7c28a6053189?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      //Project URL - Add Your Project Url Here
+      url: "https://pprewitt.github.io/TrailsApp/",
+    },
+    {
+      id: 2, //DO NOT CHANGE THIS (Please)😅
+      title: "Book Finder",
+      service: "Website Development",
+      imageSrc:
+        "https://images.unsplash.com/photo-1544716278-ca5e3f4abd8c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      url: "https://book-worms-book-finder.herokuapp.com/",
+    },
+    {
+      id: 3, //DO NOT CHANGE THIS (Please)😅
+      title: "Burger App",
+      service: "Website Development",
+      imageSrc:
+        "https://images.unsplash.com/photo-1530554764233-e79e16c91d08?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      url: "https://burger-app-jorich.herokuapp.com/",
+    },
+    {
+      id: 4, //DO NOT CHANGE THIS (Please)😅
+      title: "Workout Tracker",
+      service: "Website Development",
+      imageSrc:
+        "https://images.unsplash.com/photo-1517836357463-d25dfeac3438?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      url:
+        "https://workout-tracker-jorich.herokuapp.com/?id=5fa20d8fe149100017db825f",
+    },
+    {
+      id: 5,
+      title: "Note Taker",
+      service: "Website Development",
+      imageSrc: "https://images.unsplash.com/photo-1483546416237-76fd26bbcdd1?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+      url: "https://note-taker-jorich.herokuapp.com/",
+    },
+    {
+        id: 6,
+        title: "Budget Tracker",
+        service: "Website Development",
+        imageSrc: "https://images.unsplash.com/photo-1454165804606-c3d57bc86b40?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=500&q=60",
+        url: "https://budget-tracker-jorich.herokuapp.com/",
+      },
+  ],
+  social: [
+    // Add Or Remove The Link Accordingly
+    { name: "Github", url: "https://github.com/hannahjorich" },
+    {
+      name: "LinkedIn",
+      url: "https://www.linkedin.com/in/hannah-jorich-09550586/",
+    },
+    {
+      name: "Resume",
+      url: "https://hannahjorich.github.io/Responsive-Portfolio/assets/Resume_JORICH.pdf",
+    },
+  ],
+};
